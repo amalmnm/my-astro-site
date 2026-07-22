9a2feb5c4badf6de1110cf5523e5019abcdb28aa
Sumit Kumar <sumitkumar@192.168.1.3>
2026-07-22 21:06:43 +0530

    Update navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://amalmnm.github.io',
-  base: '/my-astro-site',
+  base: import.meta.env.PROD ? '/my-astro-site' : '/',
 
   vite: {
     plugins: [tailwindcss()]
